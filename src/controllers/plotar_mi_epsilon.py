92fb5fefb8c78eac92a54fdb1b6914e01ba7fe26
Lucas Nascimento <lucasbnas435@gmail.com>
2025-05-15 21:35:50 -0300

    feat: create plotar_mi_epsilon controller

---
+++ src/controllers/plotar_mi_epsilon.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from flask import render_template
+
+
+def plotar_mi_epsilon(
+    hash_arquivo: str,
+    csv_filename: str,
+    undfrequencia: str,
+    coaxial: bool = False,
+):
+    try:
+        dados = np.loadtxt(
+            f"./pythonPaginaINPE/static/files/txt_gerado/mm_{hash_arquivo}.txt"
+        )
+    except Exception as e:
+        print(f"Erro ao recuperar arquivo txt para plotar mi e epsilon: {e}")
+        raise
+
+    print("Extraindo dados de mi e epsilon")
+    frequencia = dados[:, 0]
+    e = dados[:, 1]
+    e_prime = dados[:, 2]
+    u = dados[:, 3]
+    u_prime = dados[:, 4]
+
+    print("Iniciando plotagem do gráfico de permissividade elétrica")
+    fig = plt.figure(figsize=(10, 5))
+    ax = fig.add_subplot(1, 2, 1)
+    plt.plot(frequencia, e, label="\u03b5'")
+    plt.plot(frequencia, e_prime, label="\u03b5''")
+    plt.xlabel(f"Frequência ({undfrequencia})")
+    plt.ylabel("Permissividade Elétrica Relativa (\u03b5\u1d63)")
+    plt.title(
+        "Arquivo: "
+        + csv_filename
+        + "\nGráfico \u03b5' vs. \u03b5'' em função da Frequência"
+    )
+    plt.legend()
+    plt.grid(True)
+    print("Plotagem do gráfio de permissividade elétrica finalizada")
+
+    print("Iniciando plotagem do gráfico de permeabilidade magnética")
+    ax = fig.add_subplot(1, 2, 2)
+    plt.plot(frequencia, u, label="\u00b5'")
+    plt.plot(frequencia, u_prime, label="\u00b5''")
+    plt.xlabel(f"Frequência ({undfrequencia})")
+    plt.ylabel("Permeabilidade Magnética Relativa (\u00b5\u1d63)")
+    plt.title(
+        "Arquivo: "
+        + csv_filename
+        + "\nGráfico \u00b5' vs. \u00b5'' em função da Frequência"
+    )
+    plt.legend()
+    plt.grid(True)
+    print("Plotagem do gráfico de permeabilidade magnética finalizada")
+
+    caminho_imagem = (
+        f"./pythonPaginaINPE/static/images/mi_epsilon_{hash_arquivo}.png"
+    )
+    fig.savefig(caminho_imagem)
+
+    rota_informacoes = "/informacoes"
+    if coaxial:
+        rota_informacoes = "/informacoescoaxial"
+
+    return render_template("grafico_mi_epsilon.html")
