e2f12aa77a86813ba434d5c283953587eea01bfe
Lucas Nascimento <lucasbnas435@gmail.com>
2025-05-17 12:11:50 -0300

    feat: invoke calcular_rl in plotar_rl_espessura_fixa

--- src/controllers/plotar_rl_espessura_fixa.py
+++ src/controllers/plotar_rl_espessura_fixa.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
-import numpy as np
 from flask import render_template
+
+from src.use_cases.calcular_rl import calcular_rl
 
 
 def plotar_rl_espessura_fixa(
@@ -15,75 +16,31 @@ def plotar_rl_espessura_fixa(
     with open(caminho_arquivo_txt, "r") as arquivo_txt:
         conteudo_arquivo_txt = arquivo_txt.readlines()
 
-    # Ajuste da Referencia de L1 e L2
-    # [m] Espessura da amostra (Livro chama de L)
-    d = espessura_amostra * 1e-3
-
-    # CONSTANTES
-    c = 2.998e8  # [m/s] #velocidade da Luz no vacuo
-
-    # Vetores - 1
-    F = []  # frequencia [Hz] DE CALCULO
-    F_grafic = []  # FREQUENCIA PARA PLOTAR EM [GHz]
-
-    # Vetores - 2
-    er_r = []  # permissividade real
-    er_i = []  # permissividade imaginaria
-    ur_r = []  # permeabilidade real
-    ur_i = []  # permeabilidade imaginaria
-
-    EX = []
-    UX = []
-
-    # Vetores - 3
-    s11_v = []  # [a.u]
-
-    for n, linha in enumerate(conteudo_arquivo_txt):
-        dados = linha.split("\t")
-        # frequencia
-        f = float(dados[0]) * 1e9
-        F.append(f)
-        F_grafic.append(f / 1e9)
-
-        # Permissividade NRW
-        ex = float(dados[1]) + 1j * float(dados[2])
-        er_r.append(ex.real)
-        er_i.append(ex.imag)
-        EX.append(ex)
-
-        # Permeabilidade NRW
-        ux = float(dados[3]) + 1j * float(dados[4])
-        ur_r.append(ux.real)
-        ur_i.append(ux.imag)
-        UX.append(ux)
-
-        # *********************Calculo da Refletividade (RL)***************
-        # Calcular impedância de entrada
-        z = (50 * (UX[n] / EX[n]) ** (1.0 / 2.0)) * np.tanh(
-            1j * (2 * np.pi * d * f / c) * ((UX[n] * EX[n]) ** (1.0 / 2.0))
-        )
-        db = -20 * np.log10(
-            abs((z - 50) / (z + 50))
-        )  # [dB] somente para voltagem
-        s11_v.append(round(db, 5))
+    frequencias, s11_v = calcular_rl(
+        conteudo_arquivo_txt=conteudo_arquivo_txt,
+        espessura_amostra=espessura_amostra,
+    )
 
     if baixar_grafico:
         # Colocar comando para retornar arquivo
-        espessura = str(round(d / 1e-3, 2))
         with open(
-            f"./pythonPaginaINPE/static/files/saidas/saidas_{hash_arquivo}/mm_{espessura}mm.txt",
+            f"./pythonPaginaINPE/static/files/saidas/saidas_{hash_arquivo}/mm_{round(espessura_amostra, 2)}mm.txt",
             "w",
         ) as arquivo_dados_grafico:
             # Título
             arquivo_dados_grafico.write(f"Freq {unidade_frequencia} RL(dB)")
             # Dados do gráfico
-            for i in range(len(F)):
-                arquivo_dados_grafico.write(f"{F[i]:.2f} {s11_v[i]:.2f}")
-        return f"mm_{espessura}mm.txt"
+            for i in range(len(frequencias)):
+                arquivo_dados_grafico.write(
+                    f"{frequencias[i]:.2f} {s11_v[i]:.2f}"
+                )
+        return f"mm_{round(espessura_amostra, 2)}mm.txt"
 
     # Plotando grafico
     fig = plt.figure(figsize=(10, 5))
-    plt.plot(F, s11_v, label=str(float(d) * 1e3))  # Dados para serem plotados
+    plt.plot(
+        frequencias, s11_v, label=str(espessura_amostra)
+    )  # Dados para serem plotados
     plt.legend(title="Espessura (mm)")
     plt.xlabel(f"Frequência ({unidade_frequencia})")
     plt.ylabel("Perda por Reflexão (dB)")
