0b145ed12122f8c140fc519e570eb9c5761a2e8f
Lucas Nascimento <lucasbnas435@gmail.com>
2025-09-29 23:03:59 -0300

    refactor: delete obsolete folder

--- original-src/gerar_hash.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-import hashlib
-
-def get_hash(text_for_hash):
-    text_for_hash = str(datetime.now()) + text_for_hash
-    hash = hashlib.sha256(text_for_hash.encode()).hexdigest()[:10]
-
-    return hash
