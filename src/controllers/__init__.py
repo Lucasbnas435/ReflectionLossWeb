9a5be4b871a2d9047967ae09be3aade84df95f9b
Lucas Nascimento <lucasbnas435@gmail.com>
2025-09-29 22:31:44 -0300

    feat: create enviar_arquivo controller

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,3 +1,4 @@
+from .enviar_arquivo import enviar_arquivo_bp
 from .informacoes import informacoes_bp
 from .plotar_mu_epsilon import plotar_mu_epsilon_bp
 from .plotar_rl_espessura_dinamica import plotar_rl_espessura_dinamica_bp
@@ -6,6 +7,7 @@ from .plotar_rl_espessura_variavel import plotar_rl_espessura_variavel_bp
 
 
 def register_controllers(app):
+    app.register_blueprint(enviar_arquivo_bp)
     app.register_blueprint(informacoes_bp)
     app.register_blueprint(plotar_mu_epsilon_bp)
     app.register_blueprint(plotar_rl_espessura_fixa_bp)
---
+++ src/controllers/enviar_arquivo.py
@@ -0,0 +1,57 @@
+import os
+
+from flask import (
+    Blueprint,
+    current_app,
+    redirect,
+    render_template,
+    session,
+    url_for,
+)
+from werkzeug.utils import secure_filename
+
+from src.forms.upload_file_form import UploadFileForm
+from src.models.dados_vna import DadosVna
+from src.utils.gerar_identificador import gerar_identificador
+
+enviar_arquivo_bp = Blueprint("enviar_arquivo", __name__)
+
+
+@enviar_arquivo_bp.route("/enviar-arquivo", methods=["GET", "POST"])
+def enviar_arquivo():
+    form = UploadFileForm()
+
+    if form.validate_on_submit():
+        arquivo = form.file.data
+        identificador_arquivo = gerar_identificador()
+        arquivo.save(
+            os.path.join(
+                current_app.root_path,
+                f"{current_app.config["UPLOAD_FOLDER"]}/csv_enviado",
+                secure_filename(f"{identificador_arquivo}.csv"),
+            )
+        )
+
+        dados_vna = DadosVna(identificador_arquivo=identificador_arquivo)
+        dados_vna.ler_csv()
+        dados_vna.gerar_arquivo_txt()
+
+        dados_arquivo = {
+            "nome_arquivo_csv": arquivo.filename,
+            "identificador_arquivo": dados_vna.identificador_arquivo,
+            "caminho_arquivo_csv": dados_vna.caminho_arquivo_csv,
+            "caminho_arquivo_txt": dados_vna.caminho_arquivo_txt,
+            "frequencia_corte": dados_vna.frequencia_corte,
+            "unidade_frequencia": dados_vna.unidade_frequencia,
+            "comprimento_suporte_amostra": dados_vna.comprimento_suporte_amostra,
+            "distancia_amostra": dados_vna.distancia_amostra,
+            "espessura_amostra": dados_vna.espessura_amostra,
+            "ifbw": dados_vna.ifbw,
+            "power": dados_vna.power,
+            "nome_banda": dados_vna.nome_banda,
+        }
+
+        session.update(dados_arquivo)
+
+        return redirect(url_for("informacoes.informacoes"))
+    return render_template("tela_envio_de_arquivo.html", form=form)
