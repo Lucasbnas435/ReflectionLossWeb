344455e4af1e5cefac4655e19dde49eb875cbcae
Lucas Nascimento <lucasbnas435@gmail.com>
2025-09-29 22:17:11 -0300

    feat: create informacoes controller

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,3 +1,4 @@
+from .informacoes import informacoes_bp
 from .plotar_mu_epsilon import plotar_mu_epsilon_bp
 from .plotar_rl_espessura_dinamica import plotar_rl_espessura_dinamica_bp
 from .plotar_rl_espessura_fixa import plotar_rl_espessura_fixa_bp
@@ -5,6 +6,7 @@ from .plotar_rl_espessura_variavel import plotar_rl_espessura_variavel_bp
 
 
 def register_controllers(app):
+    app.register_blueprint(informacoes_bp)
     app.register_blueprint(plotar_mu_epsilon_bp)
     app.register_blueprint(plotar_rl_espessura_fixa_bp)
     app.register_blueprint(plotar_rl_espessura_variavel_bp)
---
+++ src/controllers/informacoes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, session
+
+informacoes_bp = Blueprint("informacoes", __name__)
+
+
+@informacoes_bp.route("/informacoes", methods=["GET", "POST"])
+def informacoes():
+    informacoes_extraidas = {
+        "nome_arquivo_csv": session.get(
+            "nome_arquivo_csv", "Nome não encontrado"
+        ),
+        "frequencia_corte": session.get("frequencia_corte", ""),
+        "unidade_frequencia": session.get("unidade_frequencia", ""),
+        "comprimento_suporte_amostra": session.get(
+            "comprimento_suporte_amostra", ""
+        ),
+        "distancia_amostra": session.get("distancia_amostra", ""),
+        "espessura_amostra": session.get("espessura_amostra", ""),
+        "ifbw": session.get("ifbw", ""),
+        "power": session.get("power", ""),
+        "nome_banda": session.get("nome_banda", ""),
+    }
+    return render_template("tela_informacoes.html", **informacoes_extraidas)
