7dc38dc4ff1a8ed43f5f7ad4234af01123f64d50
Lucas Nascimento <lucasbnas435@gmail.com>
2025-09-29 22:47:09 -0300

    feat: create home controller

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,4 +1,5 @@
 from .enviar_arquivo import enviar_arquivo_bp
+from .home import home_bp
 from .informacoes import informacoes_bp
 from .plotar_mu_epsilon import plotar_mu_epsilon_bp
 from .plotar_rl_espessura_dinamica import plotar_rl_espessura_dinamica_bp
@@ -8,6 +9,7 @@ from .plotar_rl_espessura_variavel import plotar_rl_espessura_variavel_bp
 
 def register_controllers(app):
     app.register_blueprint(enviar_arquivo_bp)
+    app.register_blueprint(home_bp)
     app.register_blueprint(informacoes_bp)
     app.register_blueprint(plotar_mu_epsilon_bp)
     app.register_blueprint(plotar_rl_espessura_fixa_bp)
